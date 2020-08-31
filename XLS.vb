faa28d969e807b870d5f9d473871450bc3c6483d
sipocz <30761411+sipocz@users.noreply.github.com>
2020-08-31 20:50:00 +0200

    end

--- XLS.vb
+++ XLS.vb
@@ -35,14 +35,28 @@ Dim out As String
 If InStr(str_input, str_selector) Then
 out = FormatDate(FindNum(str_input))
 Else
-out = FormatDate("99991231")
+out = FormatDate("99991130")
 End If
 
 CheckaString = out
 
 End Function
 
-Public Function GetaRange(r as range) As String 
-for 
+Public Function selectMinDate(input_range As Range, str_selector As String) As String
+y = input_range.Rows.Count
+x = input_range.Columns.Count
+
+minstr = "9999-12-01"
+
+For Each cell In input_range
+    
+    numstr = CheckaString(cell.Value, str_selector)
+    MsgBox (numstr + "--" + minstr)
+    If numstr < minstr Then
+    minstr = numstr
+    
+    End If
+    selectMinDate = minstr
+Next
 
 End Function
